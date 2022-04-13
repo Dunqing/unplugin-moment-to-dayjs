59df283cf4b377dc24899dcf1619db7c782238f3
dengqing <1247748612@qq.com>
2022-04-13 18:54:43 +0800

    styles: clean code

--- examples/rollup/rollup.config.ts
+++ examples/rollup/rollup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import Unplugin from 'unplugin-moment-to-dayjs/rollup'
-console.log('🚀 ~ file: rollup.config.ts ~ line 4 ~ Unplugin', Unplugin, Unplugin())
 
 export default defineConfig({
   input: 'src/index.js',
